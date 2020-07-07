895d97cb820b0e04cb005cb4b5b1381ed0aa26bb
Pablo Silva <pablossp@hotmail.com>
2020-07-06 20:06:28 -0700

    Added FileVersion support

--- src/file-version.ts
+++ src/file-version.ts
@@ -1,13 +1,12 @@
 export type FileVersion =
-    | 0x03 // dBase III without memo file
-    | 0x83 // dBase III with memo file
-    | 0x8b // dBase IV with memo file
-    | 0x30 // Visual FoxPro 9 without memo file
-;
+  | 0x03 // dBase III without memo file
+  | 0x83 // dBase III with memo file
+  | 0x8b // dBase IV with memo file
+  | 0x30 // Visual FoxPro 9 without memo file
+  | 0x32 // Visual FoxPro Dbase 50
 
-
-
-
-export function isValidFileVersion(fileVersion: number): fileVersion is FileVersion {
-    return [0x03, 0x83, 0x8b, 0x30].includes(fileVersion);
+export function isValidFileVersion(
+  fileVersion: number
+): fileVersion is FileVersion {
+  return [0x03, 0x83, 0x8b, 0x30, 0x32].includes(fileVersion)
 }
